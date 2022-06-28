20046df0e97e3f43ee7fb77358ad2b92dc699dad
Lorenzo C. Bianchi <bianchi92@hotmail.com>
2022-06-28 20:54:52 -0300

    Add o exercícios 01 e 02

--- aula01/exercicios.js
+++ aula01/exercicios.js
@@ -14,21 +14,20 @@
 
 const prompt = require('prompt-sync')();
 
-// 3) 
-// const dataAtual = new Date();
-// const anoAtual = dataAtual.getFullYear();
+//// 3) 
+const dataAtual = new Date();
+const anoAtual = dataAtual.getFullYear();
 
-// console.log("Digite ano de nacimento: ");
-// const num = +prompt();
+console.log("Digite ano de nacimento: ");
+const num = +prompt();
 
-// const ano = anoAtual - num; 
-// const mes = ano * 12;
-// const dia = mes * 30; 
+const ano = anoAtual - num; 
+const mes = ano * 12;
+const dia = mes * 30; 
 
-// console.log(`Você têm ${ano} anos, já passou por ${mes} meses e viveu ${dia} dias!`);
+console.log(`Você têm ${ano} anos, já passou por ${mes} meses e viveu ${dia} dias!`);
 
-// 4) 
-
+//// 4) 
 console.log("Digite um valor inteiro: ");
 const valor = parseInt(prompt());
 
@@ -37,3 +36,42 @@ const meses = parseInt((valor - (anos * 365))/30);
 const dias = parseInt((valor - (anos * 365) - (meses * 30)));
 
 console.log(`Você têm ${anos} anos, ${meses} mes(es) e ${dias} dia(as)!`);
+
+//// 1) 
+console.log("Por favor, digite o valor da nota A:")
+const notaA = +prompt();
+console.log();
+console.log("Por favor, digite o valor da nota B:")
+const notaB = +prompt();
+console.log();
+const media = ((notaA * 4) + (notaB * 6))/10;
+
+console.log(`A média do aluno foi: ${media}`);
+
+//// 2)
+console.log("Por favor, digite o nome da peça que você quer comprar:");
+const peca01 = prompt();
+console.log(`Por favor, digite o valor de ${peca01}:`);
+const valor01 = +prompt();
+console.log(`Por favor, digite a quantidade da ${peca01} que você quer comprar:`);
+const qtd01 = +prompt();
+console.log();
+
+console.log("Por favor, digite o nome da segunda peça que você quer comprar:");
+const peca02 = prompt();
+console.log(`Por favor, digite o valor de ${peca02}:`);
+const valor02 = +prompt();
+console.log(`Por favor, digite a quantidade de ${peca02} que você quer comprar:`);
+const qtd02 = +prompt();
+console.log();
+
+const totalP01 = valor01 * qtd01;
+const totalP02 = valor02 * qtd02;
+const totalCompra = totalP01 + totalP02;
+
+console.log(`O valor total de ${peca01} ficou: ${totalP01} reais;`);
+console.log(`O valor total de ${peca02} ficou: ${totalP02} reais;`);
+console.log(`O valor total da sua compra foi de: ${totalCompra} reais.`);
+
+
+
