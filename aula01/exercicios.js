77e016bc932cd3d0dbbfbcf3f9273d8f2db6dcd3
Lorenzo C. Bianchi <bianchi92@hotmail.com>
2022-06-28 21:41:41 -0300

    Add o prettierrc

--- aula01/exercicios.js
+++ aula01/exercicios.js
@@ -14,54 +14,60 @@
 
 const prompt = require('prompt-sync')();
 
-//// 3) 
+//// 3)
 const dataAtual = new Date();
 const anoAtual = dataAtual.getFullYear();
 
-console.log("Digite ano de nacimento: ");
+console.log('Digite ano de nacimento: ');
 const num = +prompt();
 
-const ano = anoAtual - num; 
+const ano = anoAtual - num;
 const mes = ano * 12;
-const dia = mes * 30; 
+const dia = mes * 30;
 
-console.log(`Você têm ${ano} anos, já passou por ${mes} meses e viveu ${dia} dias!`);
+console.log(
+    `Você têm ${ano} anos, já passou por ${mes} meses e viveu ${dia} dias!`,
+);
 
-//// 4) 
-console.log("Digite um valor inteiro: ");
+//// 4)
+console.log('Digite um valor inteiro: ');
 const valor = parseInt(prompt());
 
 const anos = parseInt(valor / 365);
-const meses = parseInt((valor - (anos * 365))/30);
-const dias = parseInt((valor - (anos * 365) - (meses * 30)));
+const meses = parseInt((valor - anos * 365) / 30);
+const dias = parseInt(valor - anos * 365 - meses * 30);
 
 console.log(`Você têm ${anos} anos, ${meses} mes(es) e ${dias} dia(as)!`);
 
-//// 1) 
-console.log("Por favor, digite o valor da nota A:")
+//// 1)
+console.log('Por favor, digite o valor da nota A:');
 const notaA = +prompt();
 console.log();
-console.log("Por favor, digite o valor da nota B:")
+console.log('Por favor, digite o valor da nota B:');
 const notaB = +prompt();
 console.log();
-const media = ((notaA * 4) + (notaB * 6))/10;
+const media = (notaA * 4 + notaB * 6) / 10;
 
 console.log(`A média do aluno foi: ${media}`);
 
 //// 2)
-console.log("Por favor, digite o nome da peça que você quer comprar:");
+console.log('Por favor, digite o nome da peça que você quer comprar:');
 const peca01 = prompt();
 console.log(`Por favor, digite o valor de ${peca01}:`);
 const valor01 = +prompt();
-console.log(`Por favor, digite a quantidade da ${peca01} que você quer comprar:`);
+console.log(
+    `Por favor, digite a quantidade da ${peca01} que você quer comprar:`,
+);
 const qtd01 = +prompt();
 console.log();
 
-console.log("Por favor, digite o nome da segunda peça que você quer comprar:");
+console.log('Por favor, digite o nome da segunda peça que você quer comprar:');
 const peca02 = prompt();
 console.log(`Por favor, digite o valor de ${peca02}:`);
 const valor02 = +prompt();
-console.log(`Por favor, digite a quantidade de ${peca02} que você quer comprar:`);
+console.log(
+    `Por favor, digite a quantidade de ${peca02} que você quer comprar:`,
+);
 const qtd02 = +prompt();
 console.log();
 
@@ -72,6 +78,3 @@ const totalCompra = totalP01 + totalP02;
 console.log(`O valor total de ${peca01} ficou: ${totalP01} reais;`);
 console.log(`O valor total de ${peca02} ficou: ${totalP02} reais;`);
 console.log(`O valor total da sua compra foi de: ${totalCompra} reais.`);
-
-
-
---
+++ aula02/index.js
@@ -0,0 +1,29 @@
+//=== Exercícios Extras ===\\
+
+/**
+1) Frase na tela - Implemente um programa que escreve na tela a frase “O primeiro programa a gente nunca esquece!”.
+
+2) Etiqueta - Elabore um programa que escreve seu nome completo na primeira linha, seu endereço na segunda, o CEP na terceira e o telefone na quarta.
+
+3) Letra de Música - Faça um programa que mostre na tela uma letra de música que você gosta.
+
+4) Tabela de notas - Você foi contratado ou contratada por uma escola para fazer o sistema de boletim dos alunos. Como primeiro passo, escreva um programa que produza a seguinte saída:
+
+  ALUNO (A) ****    NOTA
+  ALINE              9.0
+  MÁRI0              DEZ
+  SÉRGIO             4.5
+  SHIRLEY            7.
+  
+5) Faça um programa de cadastro de clientes que mostre um menu de opções e permita com que a pessoa escolha umas das opções, exibindo qual foi a opção escolhida.
+
+6) Calculadora de Dano - Escreva um programa que receba dois valores digitados pelo usuário: quantidade de vida de um monstro (entre 10 e 50) e valor do ataque do jogador por turno (entre 5 e 10). Baseado nos valores digitados, exiba a quantidade de turnos que o jogador irá demorar para conseguir derrotar o monstro (o jogador irá derrotar o monstro em 8 turnos).
+
+7) E os 10% do garçom? - Defina uma variável para o valor de uma refeição que custou R$ 42,54. Na sequência, defina uma variável para o valor da taxa de serviço que é de 10%. Por fim defina uma variável que será responsável pelo cálculo do valor total da conta e exiba-o no console com a seguinte formatação: R$99.99 (valor com duas casas decimais).
+
+8) Qual o valor do troco? - Crie um programa que calcule o troco de uma compra no valor de R$100,98 e que foi paga com R$150,00. O valor do troco deve ser exibido no console.
+
+9) Conversor de moedas - Crie um programa que solicite um valor em real ao usuário e converta esse valor, para dólar.
+
+ 
+*/
