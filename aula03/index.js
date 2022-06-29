74eaeedc45190e57e1acb44f026f107d56ed6bfa
Lorenzo C. Bianchi <bianchi92@hotmail.com>
2022-06-29 20:33:31 -0300

    Add material de Booleanos, Comparação e Diferencial
    Falta a parte de Comparação.

---
+++ aula03/index.js
@@ -0,0 +1,60 @@
+/** Booleanos:
+
+  * Um booleano, é um tipo de dado lógico que pode ter apenas um de dois valores possíveis: verdadeiro ou falso. Por exemplo, em JavaScript, condicionais booleanas são usadas para decidir quais trechos do código serão executados ou repetidas.
+
+    Com isso é possível fazer comparações e obter como resultado true ou false.
+
+    - Variável terá o mesmo tipo que sua comparação* - variável = boolean
+    - Toda informação terá um "valor" e essa tera um "tipo" -> (true - boolean); (10, number)  
+*/
+const prompt = require('prompt-sync')();
+
+console.log('--- Boolean ---');
+
+const comparacao = 5 > 3;
+console.log(comparacao);
+console.log(typeof comparacao);
+console.log();
+
+/** Atribuição, Comparações de igualdade (= , ==, ===) e Diferenças:
+  
+  = ---> É usado para atribuir um valor; 
+  
+  == --> Compara se os dois valores são iguais, independente do tipo;
+  
+  === -> Compara se os valores são iguais e do mesmo tipo;
+  
+  != -->  Verifica se um valor é diferente de outro;
+  
+  !== ->  Verifica se um valor e tipo é diferente de outro;
+
+ */
+
+console.log('--- Atribuição e Comparações de igualdade (= , ==, ===): ---');
+
+console.log('==');
+const comparacao1 = 50 == '50'; // true
+const comparacao2 = 50 == 50; // true
+console.log(comparacao1);
+console.log(comparacao2);
+
+console.log('===');
+const comparaTipo1 = 50 === '50'; // false
+const comparaTipo2 = 50 === 50; // true
+console.log(comparaTipo1);
+console.log(comparaTipo2);
+
+console.log('!=');
+const comparaString = 'Blue' != 'Blue';
+console.log(comparaString);
+
+console.log('!==');
+const comparaString02 = '12' !== 12;
+console.log(comparaString02);
+console.log();
+
+/** Condicionais:
+  
+
+
+ */
