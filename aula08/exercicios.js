4c1e6f5b363097f4911797d610b41896722ccfd7
Lorenzo C. Bianchi <bianchi92@hotmail.com>
2022-07-07 20:11:38 -0300

    Add resolução exercício 02 da aula 08

--- aula08/exercicios.js
+++ aula08/exercicios.js
@@ -9,29 +9,44 @@ const prompt = require('prompt-sync')();
     [ 1, 2, 3, 4 ]
     [ 2, 3, 4, 5 ]
     
-    2) Faça um Programa que peça a idade e a altura de 5 pessoas, armazene cada informação no seu respectivo array. Imprima a média de idade e de altura.
+    2) Escreva um programa que peça um número e escreva o fatorial dele. Exemplo 6! = 6 x 5 x 4 x 3 x 2 x 1 = 720
     
-    3) Faça um programa em que o usuário escolha a quantidade de alunos a serem cadastrados e peça o nome e nota de cada um. Armazene as informações em duas listas separadas e no final imprima o nome e a nota correspondente de cada aluno, e a média total da notas.
+    3) Faça um programa que peça um número e imprima toda a tabuada dele do 1 ao 10.
     
-    4) Faça um programa que simule um lançamento de dados. Lance o dado 100 vezes e armazene os resultados em um array. Depois, mostre quantas vezes cada valor foi conseguido
+    4) Faça um programa que carregue uma lista com os modelos de cinco carros. Carregue uma outra lista com o consumo desses carros, isto é, quantos quilômetros cada um desses carros faz com um litro de combustível. Calcule e mostre:
+        
+        > O modelo do carro mais econômico;
 
- */
+        > Quantos litros de combustível cada um dos carros cadastrados consome para percorrer uma distância de 1000 quilômetros e quanto isto custará, considerando um que a gasolina custe R$ 5,50 o litro.
+
+*/
 
 //// 1)
-const matriz = [];
-console.log('\nQuantas linhas você quer?');
-const linha = +prompt(`R:`);
-console.log('\nQuantas colunas você quer?');
-const coluna = +prompt(`R:`);
+// const matriz = [];
+// console.log('\nQuantas linhas você quer?');
+// const linha = +prompt(`R:`);
+// console.log('\nQuantas colunas você quer?');
+// const coluna = +prompt(`R:`);
 
-for (let i = 0; i < linha; i++) {
-    let linhaInterna = [];
-    for (let j = 0; j < coluna; j++) {
-        linhaInterna.push(j + i);
-    }
-    matriz.push(linhaInterna);
+// for (let i = 0; i < linha; i++) {
+//     let linhaInterna = [];
+//     for (let j = 0; j < coluna; j++) {
+//         linhaInterna.push(j + i);
+//     }
+//     matriz.push(linhaInterna);
+// }
+
+// for (let i of matriz) {
+//     console.log(i);
+// }
+
+////2)
+
+console.log(`Qual número fatorial você quer saber: `);
+const escolha = +prompt(`R:`);
+let fatorial = 1;
+for (let i = 1; escolha >= i; i++) {
+    fatorial = fatorial * i;
 }
 
-for (let i of matriz) {
-    console.log(i);
-}
+console.log(fatorial);
---
+++ aula09/index.js
@@ -0,0 +1,9 @@
+console.clear();
+const prompt = require('prompt-sync')();
+
+//=== Exercícios de Fixação ===\\
+
+/**
+
+ 
+*/
