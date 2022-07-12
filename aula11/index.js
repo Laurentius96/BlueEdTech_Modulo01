756293a1c09f994c68050003e2ae0a9d0f0824c8
Lorenzo C. Bianchi <bianchi92@hotmail.com>
2022-07-11 21:13:09 -0300

    Add exemplo 01 aula 11

--- aula11/index.js
+++ aula11/index.js
@@ -7,6 +7,18 @@ const prompt = require('prompt-sync')();
 
     É como se ensinássemos para o computador uma série de comandos que queremos executar chamando apenas por um nome. Isso é muito útil quando precisamos executar um certo código várias vezes durante a execução do nosso programa, então ao invés de repetir várias vezes esses códigos, criamos uma função para facilitar.
 
-
+    Para que ele execute os comandos, é necessário chamar a função, e fazemos isso apenas pelo nome dela seguido de ().
 
  */
+
+//// Exemplo 01:
+
+console.log(`\nExemplo 01`);
+function saudacao() {
+    console.log('Seja bem vindo(a) Bluemer!');
+    console.log('Essa é a aula 11 do nosso curso.');
+    console.log();
+}
+
+saudacao();
+
